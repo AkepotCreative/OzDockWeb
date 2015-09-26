73cf865ff3d59336612bafdfc6d8ff4cfa7774d6
Cole Lanum <ccobra3000@users.noreply.github.com>
2015-09-26 07:54:13 -0500

    Mobile Layout
    Added the mobile layout and functions.

--- ozdock.com/scripts/default.js
+++ ozdock.com/scripts/default.js
@@ -7,7 +7,14 @@ var temptimer; // Timer to refresh temperature
 var lockmap; // Lock the map to specific location
 var markers = new Array(); // Array to store map markers
 var tweets = new Array(); // Array to store listed tweets
-var tweettemplate = '<div id="{ID}" class="tweet">'
+var IsMobile;
+
+// Default settings
+var DISPLAYMAXTWEETS = 20;
+var MOBILEDISPLAYMAXTWEETS = 20;
+var LockMapPos = false;
+var LockTweetPos = false;
+var TweetTemplate = '<div id="{ID}" class="tweet">'
                 + '<img class="tweetprofileimg" src="{PROFILEIMAGE}" />'
                 + '<div class="tweetname"><a href="https://twitter.com/intent/user?screen_name={USERID}" target="_blank"><b>{NAME}</b><br /> <span class="tweetuser">@{USERID}</span></a></div>'
                 + '<div class="date">{TIME}</div>'
@@ -16,8 +23,23 @@ var tweettemplate = '<div id="{ID}" class="tweet">'
                 + '<div class="tweetopen"><a href="https://twitter.com/{USERID}/status/{ID}" target="_blank">Open</a></div>'
                 + '<hr />'
                 + '</div>';
-var LockMapPos = false;
-var LockTweetPos = false;
+var MobileTweetTemplate = '<div id="{ID}" class="MobileTweet">'
+                + '<div class="MobileTweetContent">'
+                + '<div class="MobileTweetData">'
+                + '<div class="MobileTweetProfileImage"><img src="{PROFILEIMAGE}" /></div>'
+                + '<div class="MobileTweetUser"><a href="https://twitter.com/intent/user?screen_name={USERID}" style="text-decoration:none;color:darkgrey;">{USERID}</a></div>'
+                + '<div class="MobileTweetText">{TEXT}</div>'
+                + '</div>'
+                + '<div class="MobileTweetInfo">'
+                + '<div class="MobileTweetTime">{TIME}</div>'
+                + '<div class="MobileTweetApp">{APP}</div>'
+                + '</div>'
+                + '</div>'
+                + '<div class="MobileTweetActions">'
+                + '<br /><br /><br /><br /><br />'
+                + '<br />'
+                + '</div>'
+                + '</div>';
 
 // Limited Dependencies
 // Some sites may prefer to embed the unobtrusive Web Intents pop-up Javascript inline or without a dependency to platform.twitter.com.
@@ -25,8 +47,20 @@ var LockTweetPos = false;
 // https://dev.twitter.com/web/intents
 
 $(document).ready(function () {
+    $('#SearchInput').val('');
     LoadMap();
     $('#Tweets').niceScroll();
+
+    // set default lock status
+    lockmap = true;
+
+    // check which platform is used
+    if ($('#MobileTweets').css('display') == 'none') {
+        IsMobile = false;
+    }
+    else {
+        IsMobile = true;
+    }
 
     if ('WebSocket' in window) {
         opensocket();
@@ -35,13 +69,20 @@ $(document).ready(function () {
         browsererror();
         return;
     }
-
-    // set default lock status
-    lockmap = true;
 });
 
 $(window).on('beforeunload', function () {
     ws.close();
+});
+
+$(window).resize(function () {
+    // check which platform is used
+    if ($('#MobileTweets').css('display') == 'none') {
+        IsMobile = false;
+    }
+    else {
+        IsMobile = true;
+    }
 });
 
 function opensocket() {
@@ -58,11 +99,12 @@ function opensocket() {
         cmd = msg.command;
         switch (cmd) {
             case "tweet":
-                // desktop version
-                PostTweet(msg);
-
-                // mobile version
-                // PostTweetMobile(msg);
+                if (IsMobile) {
+                    MobilePostTweet(msg);
+                }
+                else {
+                    PostTweet(msg);
+                }
                 break;
             default:
                 // unrecognized command
@@ -118,6 +160,33 @@ function LoadMap() {
     ];
     map = new google.maps.Map(document.getElementById('GoogleMap'), myOptions);
     map.setOptions({ styles: myStyles });
+
+    // Setup input for Google Maps Places API AutoComplete
+    var ActiveInputElement;
+    if ($('#MobileHeader').css('display') == 'none') {
+        ActiveInputElement = document.getElementById('SearchInput');
+    }
+    else {
+        ActiveInputElement = document.getElementById('MobileSearchBox');
+    }
+
+    var autocomplete = new google.maps.places.Autocomplete(
+        (ActiveInputElement), {
+            types: ['(cities)'],
+            componentRestrictions: { 'country': 'us' }
+        });
+    autocomplete.bindTo('bounds', map);
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function () {
+        var place = autocomplete.getPlace();
+        if (place.geometry) {
+            map.panTo(place.geometry.location);
+            map.setZoom(7);
+        } else {
+            ActiveInputElement.placeholder = 'Search';
+        }
+    });
+
     //var imageBounds = new google.maps.LatLngBounds(new google.maps.LatLng(22.652538062803, -128.620375523875420), new google.maps.LatLng(51.406626367301044, -67.517937876818));
     //var radarmap = new google.maps.GroundOverlay('http://radblast-mi.wunderground.com/cgi-bin/radar/WUNIDS_composite?maxlat=50.406626367301044&maxlon=-66.517937876818&minlat=21.652538062803&minlon=-127.620375523875420&type=N0R&frame=0&width=9775&height=4600&png=0&smooth=1&min=2&noclutter=0&rainsnow=1&nodebug=0&theext=.gif&brand=wundermap&reproj.automerc=1&merge=elev&rand=21854266', imageBounds);
     //radarmap.setMap(map);
@@ -135,7 +204,7 @@ function connectionlost() {
 function SetDefaultGeo() {
     geoacq = false;
     lat = '39.035992';
-    long = '-95.693611';
+    long = '-95.693611'; // Topeka, KS
 }
 
 function GetGeoLocation() {
@@ -160,11 +229,14 @@ function GetTemperature() {
             var temp_f = parsed_json['current_observation']['temp_f'];
             temp_f = Math.round(temp_f);
             $('#Temperature').html(temp_f + '&deg;');
+            $('#MobileTemperature').html(temp_f + '&deg;');
             if (!geoacq) {
                 $('#Temperature').attr('title', 'Your location was not found. Using default location.');
+                $('#MobileTemperature').attr('title', 'Your location was not found. Using default location.');
             }
             else {
                 $('#Temperature').attr('title', location);
+                $('#MobileTemperature').attr('title', location);
             }
         }
     });
@@ -186,20 +258,18 @@ function geofail() {
 }
 
 function PostTweet(tweet) {
-    // if more than 15 markers, remove one before adding one
-    if (markers.length > 15) {
+    // remove one before adding one if max
+    if (markers.length > DISPLAYMAXTWEETS) {
         var removedmarker = markers.shift();
         removedmarker.setMap(null);
     }
-
-    // if more than 15 tweets, remove one before adding one
-    if (tweets.length > 15) {
+    if (tweets.length > DISPLAYMAXTWEETS) {
         var removedtweet = tweets.shift();
         $("#" + removedtweet).remove();
     }
 
     var tweetid = tweet.tweetid;
-    var tweethtml = tweettemplate;
+    var tweethtml = TweetTemplate;
     tweethtml = tweethtml.replace(/\{ID\}/g, tweetid);
     tweethtml = tweethtml.replace(/\{PROFILEIMAGE\}/g, tweet.profile_image_url_https);
     tweethtml = tweethtml.replace(/\{USERID\}/g, tweet.screen_name);
@@ -227,7 +297,7 @@ function PostTweet(tweet) {
 
     // if the scroll wasn't top, move to account for added content
     if (currentscroll > 2) {
-        $('#Tweets').scrollTop(currentscroll + $Tweet.outerHeight() + 14); // trial and error 14. figure out where the extra 14px are coming from
+        $('#Tweets').scrollTop(currentscroll + $Tweet.outerHeight() + 14); // trial and error 14. figure out where the extra 14px are coming from. 9/12/2015: finding sometimes 14 isn't enough to keep scroll position. possibly outerHeight isn't returning what we want
     }
     else {
         $('#Tweets').scrollTop(0);
@@ -289,6 +359,90 @@ function PostTweet(tweet) {
         setTimeout(function () {
             marker.setAnimation(null);
         }, 2000);
+    });
+}
+
+function MobilePostTweet(tweet) {
+    // if more than 15 markers, remove one before adding one
+    if (markers.length > MOBILEDISPLAYMAXTWEETS) {
+        var removedmarker = markers.shift();
+        removedmarker.setMap(null);
+    }
+
+    // if more than 15 tweets, remove one before adding one
+    if (tweets.length > MOBILEDISPLAYMAXTWEETS) {
+        var removedtweet = tweets.shift();
+        var indexofid = removedtweet.search("id=");
+        if (indexofid >= 0) {
+            var tempstr = removedtweet.substring(indexofid + 4);
+            var indexofq = tempstr.search('"');
+            var tweetid = tempstr.substring(0, indexofq);
+            $("#" + tweetid).remove();
+        }
+    }
+
+    var tweethtml = MobileTweetTemplate;
+    tweethtml = tweethtml.replace(/\{ID\}/g, tweet.tweetid);
+    tweethtml = tweethtml.replace(/\{PROFILEIMAGE\}/g, tweet.profile_image_url_https);
+    tweethtml = tweethtml.replace(/\{USERID\}/g, tweet.screen_name);
+    tweethtml = tweethtml.replace(/\{TEXT\}/g, tweet.text);
+    tweethtml = tweethtml.replace(/\{TIME\}/g, tweet.created_at);
+    tweethtml = tweethtml.replace(/\{APP\}/g, " ");
+
+    // add to page
+    $("#MobileTweets").prepend(tweethtml);
+
+    // add to array
+    tweets.push(tweethtml);
+
+    // Clean up the coords
+    var coords = tweet.coordinates;
+    coords = coords.replace(/(\r\n|\n|\r)/gm, "");
+    coords = coords.replace("[", "");
+    coords = coords.replace("]", "");
+    coords = coords.replace(" ", "");
+    coords = coords.split(",");
+    var lng = coords[0];
+    var lat = coords[1];
+
+    // add marker to map
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: map,
+        icon: getCircle(20),
+        title: tweet.screen_name // do something with title?
+    });
+
+    // marker clicked
+    google.maps.event.addListener(marker, 'click', function () {
+        var curmarker = this; // save marker to variable because setTimeout overrides this
+        $("#MobileTweets").animate({
+            height: "270px"
+        }, 300, function () {
+            $("#MobileTweets").scrollTop(0);
+            $("#MobileTweets").getNiceScroll().remove();
+            $("#MobileTweets").css("display", "block");
+        });
+        setTimeout(function () { // run this after 4 seconds
+            curmarker.setAnimation(null);
+        }, 4000);
+    });
+
+    // zoom to new tweet if not locked
+    if (!lockmap) {
+        map.panTo(marker.getPosition());
+    }
+    // bounce map marker for 2 seconds
+    //marker.setAnimation(google.maps.Animation.BOUNCE);
+    //setTimeout(function () {
+    //    marker.setAnimation(null);
+    //}, 2000);
+
+    // add to array
+    markers.push(marker);
+
+    $("img").on("dragstart", function (event) {
+        event.preventDefault();
     });
 }
 
